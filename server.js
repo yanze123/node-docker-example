57670834cfbdb0c721dd6e9688a61729ccdb428a
yanze123 <462865314@qq.com>
2016-06-07 17:59:10 +0800

    app

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http')
+
+var server = http.createServer(function(req, res) {
+	res.writeHead(200, {
+		'Content-type' : 'text/plain'
+	})
+	res.end('Hello World!')
+})
+server.listen(8080, function() {
+	console.log('Docker DEMO with Node.js is running.')
+})
